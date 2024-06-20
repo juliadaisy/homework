2a64d2df4b202a563cc3b8967bb02dc35efea622
Yuliia Zavadska <yzavadska@allvuesystems.com>
2024-06-20 18:51:59 +0200

    Added the ability of re-entering after putting incorrect numbers or operators. Added the option to exit the calculator at any stage

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,54 +1,28 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Calculator c = new Calculator();
-        c.add(5);
-        c.sub(3);
-        c.mult(4);
-        c.add(9);
-        c.div(0);
-        c.history();
 
-
-        Calc1 C = new Calc1();
-//        C.add(5);
-//        C.sub(3);
-//        C.mult(4);
-//        C.add(9);
-////        try {
-////            C.div(2);
-////        } catch (Exception e) {
-////            System.out.println("exception");
-////        }
-//        C.div(4);
-//        C.history();
-
-
+        Number number = new Number();
         Calc1 Cl = new Calc1();
-        String s = "";
+        String s;
         Scanner sc = new Scanner(System.in);
         try {
-            System.out.println("Enter number:");
-            Cl.add(sc.nextDouble());//1 way
-            while (!s.equals("exit")) {
+
+            Cl.add(number.askForNumber());
+            while (true) {
 
                 System.out.println("Enter operator:");
                 s = sc.next();
-                while (s.equals("exit")) {
+                if (s.equals("exit")) {
                     return;
                 }
                 if (!s.equals("+") & !s.equals("-") & !s.equals("*") & !s.equals("/")) {
                     System.out.println("It's not an operator! Enter '+', '-', '*', '/'");
                     return;
                 }
-                System.out.println("Enter number:");
-                double b = sc.nextDouble();
+                double b = number.askForNumber();
                 if (s.equals("+")) {
                     System.out.println("result = " + Cl.add(b));
                 }
@@ -62,22 +36,30 @@ public class Main {
                     System.out.println("result = " + Cl.div(b));
                 }
             }
-        } catch (InputMismatchException e) {
-            System.out.println("You can't enter characters");
+        } catch (RuntimeException e) {
+            System.out.println("vse robota zakinchena");
+        }
+    }
+
+}
+
+class Number {
+    double askForNumber() {
+        while (true) {
+            try {
+
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter number:");
+                String n = sc.next();
+                if (n.equals("exit")) {
+                    throw new RuntimeException();
+                }
+                return Double.parseDouble(n);
+
+            } catch (NumberFormatException e) {
+                System.out.println("It's not a number");
+            }
         }
 
-
-//        while (true) {
-//            System.out.println("Enter operator:");
-//            s = sc.next();
-//            if (s.equals("exit"))
-//                return;//break
-//            System.out.println("Enter number:");
-//            double b = sc.nextDouble();
-//            if (s.equals("+")) {
-//                a = a + b;
-//                System.out.println("result = " + a);
-//            }
-//        }
     }
 }
