fa88d05504d875c72c2dbccb33675cbbe3d84391
Yuliia Zavadska <yzavadska@allvuesystems.com>
2024-06-22 16:25:21 +0200

    Edited the Input class using both numbers and operator functions. Replaced if/else-if statements with switch/case(was suggested within IDEA)

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -4,36 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Number number = new Number();
+        Input input = new Input();
         Calc1 Cl = new Calc1();
-        String s;
-        Scanner sc = new Scanner(System.in);
         try {
-
-            Cl.add(number.askForNumber());
+            Cl.add(input.askForNumber());
             while (true) {
-
-                System.out.println("Enter operator:");
-                s = sc.next();
-                if (s.equals("exit")) {
-                    return;
-                }
-                if (!s.equals("+") & !s.equals("-") & !s.equals("*") & !s.equals("/")) {
-                    System.out.println("It's not an operator! Enter '+', '-', '*', '/'");
-                    return;
-                }
-                double b = number.askForNumber();
-                if (s.equals("+")) {
-                    System.out.println("result = " + Cl.add(b));
-                }
-                if (s.equals("-")) {
-                    System.out.println("result = " + Cl.sub(b));
-                }
-                if (s.equals("*")) {
-                    System.out.println("result = " + Cl.mult(b));
-                }
-                if (s.equals("/")) {
-                    System.out.println("result = " + Cl.div(b));
+                String s = input.askForOperator();
+                double b = input.askForNumber();
+                switch (s) {
+                    case "+" -> System.out.println("result = " + Cl.add(b));
+                    case "-" -> System.out.println("result = " + Cl.sub(b));
+                    case "*" -> System.out.println("result = " + Cl.mult(b));
+                    case "/" -> System.out.println("result = " + Cl.div(b));
                 }
             }
         } catch (RuntimeException e) {
@@ -43,11 +25,10 @@ public class Main {
 
 }
 
-class Number {
+class Input {
     double askForNumber() {
         while (true) {
             try {
-
                 Scanner sc = new Scanner(System.in);
                 System.out.println("Enter number:");
                 String n = sc.next();
@@ -60,6 +41,24 @@ class Number {
                 System.out.println("It's not a number");
             }
         }
+    }
 
+    String askForOperator() {
+        String s;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter operator:");
+        s = sc.next();
+        if (s.equals("exit")) {
+            throw new RuntimeException();
+        }
+        while (!s.equals("+") & !s.equals("-") & !s.equals("*") & !s.equals("/")){
+            System.out.println("It's not an operator! Enter '+', '-', '*', '/'");
+            System.out.println("Enter operator:");
+            s = sc.next();
+            if (s.equals("exit")) {
+                throw new RuntimeException();
+            }
+        }
+        return s;
     }
 }
